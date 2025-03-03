3f8d30662ed4b3824b2709dae4123f9c2cb38729
medram <mramouchy@gmail.com>
2025-03-03 11:27:40 +0000

    add: README file content/docs

--- django_ninja_shield/__init__.py
+++ django_ninja_shield/__init__.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 from .base import AtomicOperation, P
+from .decorators import requires_permissions
 
 if TYPE_CHECKING:
     from django.contrib.auth.models import AbstractUser
@@ -19,6 +20,9 @@ class IsAdmin(AtomicOperation):
         return user.is_superuser
 
 
+IsSuperuser = IsAdmin  # alias
+
+
 class IsStaff(AtomicOperation):
     """A permission operation that checks if the user is a staff"""
 
@@ -33,4 +37,4 @@ class IsActive(AtomicOperation):
         return user.is_active
 
 
-__all__ = ["P", "IsAdmin", "IsStaff", "IsActive"]
+__all__ = ["P", "IsAdmin", "IsSuperuser", "IsStaff", "IsActive", "requires_permissions"]
